62a3d5b08580b383fadaaa4a7de4b1b9df19e13c
SI-Tanbir <si017490@gmail.com>
2024-11-15 04:57:47 +0600

    adding the json and fetch it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './assets/components/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,8 @@ function App() {
       <h1 className='text-6xl bg-purple-500'>Knowledge cafe</h1>
 
       </div>
+
+      <Blogs></Blogs>
       
     </>
   )
